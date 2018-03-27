bef356ee195a64345391f57538c68e13f93874ef
igioya <ignacio.gioya@gmail.com>
2018-03-27 20:02:19 -0300

    Se cambio las fechas fijas por fecha actual ya que no se esta testeando la logica de fechas

--- desapp-groupe-backend/src/test/java/model/ReservationTest.java
+++ desapp-groupe-backend/src/test/java/model/ReservationTest.java
@@ -7,8 +7,8 @@ import junit.framework.TestCase;
 public class ReservationTest extends TestCase{
 	
 	public void test_dadaUnaPublicacionUnUsuarioRealizaUnaReserva() {
-		LocalDateTime fromDate = new LocalDateTime(2018, 3, 27, 16, 30); // 27-03-2018 a las 16:30 hs
-		LocalDateTime toDate = new LocalDateTime(2018, 3, 29, 16, 30);   // 29-03-2018 a las 16:30 hs
+		LocalDateTime fromDate = new LocalDateTime();
+		LocalDateTime toDate = new LocalDateTime();
 		
 		Publication publication = new Publication();
 		
@@ -18,5 +18,6 @@ public class ReservationTest extends TestCase{
 		
 		assertEquals(publication.getReservations().size(), 1);
 	}
+	
 	
 }
