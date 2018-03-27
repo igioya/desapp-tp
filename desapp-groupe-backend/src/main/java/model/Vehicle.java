7e0cd7cf9e31d03a591b69a847871d6f1f81577d
ignaciolamandia <ignaciolamandia@gmail.com>
2018-03-27 12:42:38 -0300

    Se modifico la clase Vehicle
    se pasaron atributos a la nueva clase Publication

--- desapp-groupe-backend/src/main/java/model/Vehicle.java
+++ desapp-groupe-backend/src/main/java/model/Vehicle.java
@@ -1,33 +1,38 @@
 package model;
 
-import java.time.LocalDate;
-import java.util.Set;
-
 public class Vehicle {
 	
+	private String model;
+	private VehicleType type;
 	private Integer numberOfPassengers;
-	private String city;
-	private String retireAddress;
-	private Set<String> returnAddress;
 	private String description;
-	private String telephone;
-	private Set<LocalDate> datesAvailable;
-	private Double cost;
 	private String photo;
 	
 	public Vehicle() {}
-	
-	public Vehicle(Integer numberOfPassengers, String city, String retireAddress, Set<String> returnAddress,
-			String description, String telephone, Set<LocalDate> datesAvailable, Double cost) {
+
+	public Vehicle(String model, VehicleType type, Integer numberOfPassengers, String description, String photo) {
 		super();
+		this.model = model;
+		this.type = type;
 		this.numberOfPassengers = numberOfPassengers;
-		this.city = city;
-		this.retireAddress = retireAddress;
-		this.returnAddress = returnAddress;
 		this.description = description;
-		this.telephone = telephone;
-		this.datesAvailable = datesAvailable;
-		this.cost = cost;
+		this.photo = photo;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public VehicleType getType() {
+		return type;
+	}
+
+	public void setType(VehicleType type) {
+		this.type = type;
 	}
 
 	public Integer getNumberOfPassengers() {
@@ -38,60 +43,12 @@ public class Vehicle {
 		this.numberOfPassengers = numberOfPassengers;
 	}
 
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getRetireAddress() {
-		return retireAddress;
-	}
-
-	public void setRetireAddress(String retireAddress) {
-		this.retireAddress = retireAddress;
-	}
-
-	public Set<String> getReturnAddress() {
-		return returnAddress;
-	}
-
-	public void setReturnAddress(Set<String> returnAddress) {
-		this.returnAddress = returnAddress;
-	}
-
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public Set<LocalDate> getDatesAvailable() {
-		return datesAvailable;
-	}
-
-	public void setDatesAvailable(Set<LocalDate> datesAvailable) {
-		this.datesAvailable = datesAvailable;
-	}
-
-	public Double getCost() {
-		return cost;
-	}
-
-	public void setCost(Double cost) {
-		this.cost = cost;
 	}
 
 	public String getPhoto() {
@@ -101,5 +58,5 @@ public class Vehicle {
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
-
+	
 }
