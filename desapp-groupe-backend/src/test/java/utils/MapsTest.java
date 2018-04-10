6802d9c7893b13938191fbc5251eb5c1c4d3d8fb
igioya <ignacio.gioya@gmail.com>
2018-04-10 00:53:01 -0300

    Se creo un test de la api de google para verificar distancias

---
+++ desapp-groupe-backend/src/test/java/utils/MapsTest.java
@@ -0,0 +1,61 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MapsTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void test() throws IOException, JSONException{
+		String fromAddress = "Quilmes, Gaboto 649, Quilmes, Buenos Aires";
+		String toAddress = "Berazategui Municipalidad, Av. 14 3150, B1884CTS Gran Buenos Aires, Buenos Aires";
+		String API_KEY = "AIzaSyAzaSxsKDUNelh_OQcOyNOPJExOqJetn70";
+		String urlString = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric"
+				+ "&origins=" +fromAddress.replace(" ", "+")
+				+ "&destinations=" + toAddress.replace(" ", "+")
+				+ "&key=" + API_KEY;
+		
+		URL url = new URL(urlString);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Content-Type", "application/json");
+		
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer resp = new StringBuffer();
+		
+		while ((inputLine = in.readLine()) != null) {
+		    resp.append(inputLine);
+		}
+		
+		in.close();
+		con.disconnect();
+		
+		JSONObject json = new JSONObject(resp.toString()).getJSONArray("rows")
+ 														 .getJSONObject(0)
+														 .getJSONArray ("elements")
+														 .getJSONObject(0)
+														 .getJSONObject("distance");
+				
+		System.out.println(json.get("text").toString());
+	}
+}
