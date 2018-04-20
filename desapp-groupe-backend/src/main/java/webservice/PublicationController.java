5fbd86ae9f7a461bd77bfa6448a83a58152d5a2f
igioya <ignacio.gioya@gmail.com>
2018-04-20 13:06:08 -0300

    Se configuro cxf con un endpoint simple que devuelve un string para verificar que funciona

---
+++ desapp-groupe-backend/src/main/java/webservice/PublicationController.java
@@ -0,0 +1,24 @@
+package webservice;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import model.Publication;
+
+
+@Path("/publication")
+public class PublicationController {
+	public static final int NUMBER_OF_PUBLICATIONS = 10;
+	
+	@GET
+    @Path("/")
+    @Produces("application/json")
+    public String getPublications() {
+        
+        return "{}";
+    }
+}
