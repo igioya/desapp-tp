053592574ee93f540730b30789b504d4ef273b6b
igioya <ignacio.gioya@gmail.com>
2018-05-07 19:03:16 -0300

    Falla al injectar el HibernateUserDAO en UserService

---
+++ desapp-groupe-backend/src/main/java/webservice/UserController.java
@@ -0,0 +1,34 @@
+package webservice;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import model.User;
+import service.UserService;
+
+@Path("/users")
+public class UserController {
+	
+	private UserService userService;
+	
+	@POST
+    @Path("/new")
+    @Produces("application/json")
+	@Consumes("application/json")
+	public Response newUser(User user){
+		System.out.println("################################################");
+		System.out.println(userService.getRepository());
+		System.out.println("################################################");
+		System.exit(0);
+		userService.save(user);
+		return Response.ok().build();
+	} 
+	
+	public void setUserService(final UserService userService) {
+		this.userService = userService;
+    }
+}
