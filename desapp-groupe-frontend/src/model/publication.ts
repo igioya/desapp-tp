81ac8dab1a64fb8d6470145330a7d525c3a5b12d
ignaciolamandia <ignaciolamandia@gmail.com>
2018-05-07 23:47:15 -0300

    agregado vehiculo a la clase publication

--- desapp-groupe-frontend/src/model/publication.ts
+++ desapp-groupe-frontend/src/model/publication.ts
@@ -1,8 +1,33 @@
+import { Vehicle } from './vehicle';
+//import { User } from './user';
+
+
 export class Publication {
   id: number;
-  owner: string;
-  description: string;
-  image: string;
-  city: string;
-  price: number;
+  vehicle: Vehicle;
+  retireAddress: string;
+  returnAddress: string;
+  telephone: string;
+  costPerHour: number;
+  //reservations : any[]; //no para esta entrega
+  owner: string; //cambiar por la clase User
+
+  constructor(
+    vehicle: Vehicle,
+    retireAddress: string,
+    returnAddress: string,
+    telephone: string,
+    costPerHour: number,
+    owner: string, //cambiar por la clase User
+  	id?:number)	{
+
+      this.id = id,
+      this.vehicle = vehicle;
+      this.retireAddress = retireAddress;
+      this.returnAddress = returnAddress;
+      this.telephone = telephone;
+      this.costPerHour = costPerHour;
+      //this.reservations = [];
+      this.owner = owner;
+    }
 }
