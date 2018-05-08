f5ffb485e37116762f94d25c929aa0804bd5c233
ignaciolamandia <ignaciolamandia@gmail.com>
2018-05-07 23:50:52 -0300

    creados servicios de publicaciones y vehiculos

---
+++ desapp-groupe-frontend/src/app/services/publication.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PublicationService {
+
+  url:string = 'http://localhost:8080/publications/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllPublications():Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getPublication(id):Observable<any> {
+    return this.http.get(this.url+id);
+  }
+
+  newPublication(publication):Observable<any> {
+    return this.http.post(this.url,publication)
+  }
+
+  updatePublication(id, publication):Observable<any> {
+    return this.http.put(this.url+id,publication)
+  }
+
+  deletePublication(id, publication):Observable<any> {
+    return this.http.delete(this.url+id,publication)
+  }
+
+}
---
+++ desapp-groupe-frontend/src/app/services/vehicle.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class VehicleService {
+
+  url:string = 'http://localhost:8080/vehicles/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllVehicles():Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getVehicle(id):Observable<any> {
+    return this.http.get(this.url+id);
+  }
+
+  newVehicle(vehicle):Observable<any> {
+    return this.http.post(this.url,vehicle)
+  }
+
+  updateVehicle(id, vehicle):Observable<any> {
+    return this.http.put(this.url+id,vehicle)
+  }
+
+  deleteVehicle(id, vehicle):Observable<any> {
+    return this.http.delete(this.url+id,vehicle)
+  }
+
+}
