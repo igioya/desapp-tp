58dd686c7bc3d4c7d0ec1d898b2776c89a6ffea4
ignaciolamandia <ignaciolamandia@gmail.com>
2018-05-10 21:22:55 -0300

    agregados mensajes de error en las validaciones de publications

--- desapp-groupe-frontend/src/app/publication-form/publication-form.component.ts
+++ desapp-groupe-frontend/src/app/publication-form/publication-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Publication } from '../../model/publication';
-import { VEHICLES, PUBLICATIONS } from '../../model/data';
+import { VEHICLES, PUBLICATIONS, USERS } from '../../model/data';
 import { Vehicle } from '../../model/vehicle';
+import { User } from '../../model/user';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 import { PublicationService } from '../services/publication.service';
 
@@ -17,13 +18,16 @@ export class PublicationFormComponent {
   vehicle = Object.keys(Vehicle);
   vehicles = VEHICLES.slice(this.vehicle.length/2); //cambiar por getAll de vehiculos
 
+  owner = Object.keys(User);
+  users = USERS.slice(this.owner.length/2); //cambiar por getAll de users
+
   publication : FormGroup = this.formBuilder.group({
     vehicle : new FormControl('',Validators.required),
     retireAddress: new FormControl('',Validators.required),
     returnAddress: new FormControl('',Validators.required),
-    telephone: new FormControl('',Validators.required),
+    telephone: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(13)]),
     costPerHour: new FormControl('',Validators.required),
-    //owner: new FormControl('',Validators.required),
+    owner: new FormControl('',Validators.required),
   });
 
   constructor(private formBuilder: FormBuilder, publicationService: PublicationService) {
