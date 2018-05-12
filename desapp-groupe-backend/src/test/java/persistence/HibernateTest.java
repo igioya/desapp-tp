0be068a33694daf66533594a02e8fb1482f0ed57
ignaciolamandia <ignaciolamandia@gmail.com>
2018-05-12 16:13:03 -0300

    prueba build

--- desapp-groupe-backend/src/test/java/persistence/HibernateTest.java
+++ desapp-groupe-backend/src/test/java/persistence/HibernateTest.java
@@ -5,7 +5,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /*
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })
+@ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })*/
 public class HibernateTest {
 }
-*/
