98de33e8b90c3d3c6236f283f56023abfd0a32cb
ignaciolamandia <ignaciolamandia@gmail.com>
2018-05-12 00:45:21 -0300

    Agregado funcionamiento de i18n
    por ahora solo agregado y probado en user form

--- desapp-groupe-frontend/src/app/app.module.ts
+++ desapp-groupe-frontend/src/app/app.module.ts
@@ -9,14 +9,19 @@ import { RouterModule } from '@angular/router';
 import { UserService } from './services/user.service';
 import { VehicleService } from './services/vehicle.service';
 import { PublicationService } from './services/publication.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UsersComponent } from './users/users.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { EditPublicationComponent } from './edit-publication/edit-publication.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { EditVehicleComponent } from './edit-vehicle/edit-vehicle.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -79,6 +84,13 @@ import { EditVehicleComponent } from './edit-vehicle/edit-vehicle.component';
         component:EditPublicationComponent
       },
     ]),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [
     UserService,
