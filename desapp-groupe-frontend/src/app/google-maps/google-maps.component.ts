536f89e5c5d083bfd645192778f64840b21533cc
igioya <ignacio.gioya@gmail.com>
2018-06-17 23:42:12 -0300

    Se implemento la busqueda por mapas

--- desapp-groupe-frontend/src/app/google-maps/google-maps.component.ts
+++ desapp-groupe-frontend/src/app/google-maps/google-maps.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { PublicationService } from '../services/publication.service';
+import { AuthenticationService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { } from '@types/googlemaps';
 
 @Component({
@@ -10,20 +14,63 @@ export class GoogleMapsComponent implements OnInit {
 
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  publications
 
-  title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-
-  constructor() { }
+  constructor(private publicationService: PublicationService,
+              private authService: AuthenticationService,
+              public router: Router
+              private location: Location) { }
 
   ngOnInit() {
+    this.getPublications();
+  }
+
+  initMap(){
     var mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
+      center: new google.maps.LatLng(-34.7064781 , -58.2716504),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+    this.publications.forEach(publication => {
+      this.initMarker(publication.retireAddress, publication.id);      
+    });
+  }
+
+  getPublications() {
+    this.publicationService.getAllPublications().subscribe(data => { 
+      this.publications = data;
+      this.initMap();
+    }, err => {},
+       () => {}
+    );
+  }
+
+  initMarker(address, publicationId){
+    let contentString = "<a href='publicationDetail/"+publicationId+"'>Show publication</a> ";
+    let self = (this);
+    let geocoder = new google.maps.Geocoder();
+    let infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    geocoder.geocode({'address': address}, function(results, status) {
+      let latLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng())
+      let marker = new google.maps.Marker({
+        position: latLng,
+        map: self.map,
+        title: 'Click to zoom'
+      });
+      marker.addListener('click', function() {
+          infowindow.open(self.map, marker);
+      });
+    });
+  }
+
+  ver(){
+    console.log("HOLA")
+    this.router.navigate(['publicationDetail',publicationId]);
   }
 
 }
